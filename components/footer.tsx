30c9a59a4679d693b1c1de51e0bf5d53c685b285
Bharti <bhartivermak2012@gmail.com>
2025-09-07 22:20:39 +0530

    chore: added styles in app

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='w-full text-white bg-gray-800'>
+        <div className='mx-auto p-3 text-xs text-center'>
+           &copy; 2025 MyWebsite. All Rights Reserved 
+        </div>
+
+    </footer>
+  )
+}
+
+export default Footer
