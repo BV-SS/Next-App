0c587d2a463a8fd2649c2757116d87d377009b37
Bharti <bhartivermak2012@gmail.com>
2025-09-09 23:15:01 +0530

    add logo image and favicon using image component in next

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 
 const Navbar = () => {
   return (
-    <nav className='flex flex-row items-center pl-10 gap-5 bg-gray-800 text-white text-lg p-4'>
+    <nav className='flex flex-row items-center pl-10 gap-5 bg-gray-800 text-white text-lg p-2'>
+      <Link href="/" >
+      <Image src="/Logo.png" alt="Notes app logo" height={40} width={60} priority className='w-15 h-15 cursor-pointer inline '/>
+      <span className='text-2xl font-bold pl-3'>Notes App</span>
+      </Link>
+
+      <ul className='flex items-center ml-auto gap-6 pr-7'>
       <Link href='/'>Home</Link>
       <Link href='/notes'>Notes</Link>
       <Link href='/notes/new'>Create</Link>
-      <Link href='/about'>About</Link>
+      <Link href='/about'>About</Link>     
       <Link href='/contacts'>Contacts</Link>
+      </ul>
     </nav>
   )
 }
