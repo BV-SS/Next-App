9734294f870a09695c71e3841d5387384b66c33c
Bharti <bhartivermak2012@gmail.com>
2025-10-14 22:18:11 +0530

    create note form client and server page rendering

--- app/notes/new/page.tsx
+++ app/notes/new/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+
+// component
+import CardContainer from "@/components/CardContainer";
+import { NoteForm } from "@/components/NoteForm";
 
 const NewNote = () => {
   return (
-    <div>Create a new note</div>
-  )
-}
+    <div className="flex items-center justify-center mt-5 flex-col">
+      <CardContainer height="40rem" width="50rem">
+        <div className="flex flex-col items-start justify-evenly gap-4 p-5">
+          <h3 className="text-3xl font-bold mb-4"> Add Note</h3>
+          <NoteForm/>
+        </div>
+      </CardContainer>
+    </div>
+  );
+};
 
-export default NewNote
+export default NewNote;
